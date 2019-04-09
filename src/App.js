2e316fea71b89f409002cd8a2ff4ad6e6f3d52bd
史晓霞 <dx910114@126.com>
2019-04-09 18:21:43 +0800

    MOD：尝试微信中右上角三个点控制

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import QRCode from 'qrcode.react';
+// import wx from 'weixin-js-sdk';
 import logo from './logo.svg';
 import './App.css';
 import Fetch from './Fetch';
+let wx = require('weixin-js-sdk');
 
 class App extends Component {
+
+  componentDidMount() {
+    document.title = '这里是要修改成的新标题';
+  }
 
   makeCode() {
     Fetch.post("token?grant_type=client_credential&appid=wxd80184da41839344&secret=d6bf61d6e0285b60e29813d5eeb89f84").then((result) => {
@@ -13,6 +19,10 @@ class App extends Component {
     }).catch((err) => {
       console.log(err);
     });
+  }
+
+  onBridgeReady() {
+    WeixinJSBridge.call('hideOptionMenu');
   }
   render() {
     return (
@@ -32,7 +42,20 @@ class App extends Component {
           </a>
           <QRCode value="http://10.236.173.55:3000/" />
           <button onClick={() => {
-            this.makeCode();
+            // this.makeCode();
+            // wx.hideMenuItems({
+            //   menuList: ["menuItem:share:timeline", "menuItem:share:appMessage"]
+            // })
+            if (typeof WeixinJSBridge == "undefined") {
+              if (document.addEventListener) {
+                document.addEventListener('WeixinJSBridgeReady', this.onBridgeReady, false);
+              } else if (document.attachEvent) {
+                document.attachEvent('WeixinJSBridgeReady', this.onBridgeReady);
+                document.attachEvent('onWeixinJSBridgeReady', this.onBridgeReady);
+              }
+            } else {
+              this.onBridgeReady();
+            }
           }}>登录</button>
         </header>
       </div>
