319347c554abe732037fb63caa40f73846c7a753
史晓霞 <dx910114@126.com>
2019-04-26 17:58:41 +0800

    MOD：考虑佣金等进行计算

--- src/sys/stock/index.js
+++ src/sys/stock/index.js
@@ -32,19 +32,43 @@ const stocks = {
 class Stock extends Component {
   state = {
     type: 1, // 1：给定目标股价，计算购入手数；2：给定计划购入手数，计算最终股价
+    isAvoidFive: 2, // 1：免5；2：不免5
     result: '', // 最终计算结果
     stock: [], // 股票
   }
 
   _cal = (value) => {
-    const { type } = this.state;
-    const { curPrice, goal, stock } = value;
+    const { type, isAvoidFive } = this.state;
+    let { curPrice, goal, commissionRate } = value;
+    const { stock } = value;
+    curPrice = Number(curPrice);
+    goal = Number(goal);
+    commissionRate = Number(commissionRate) / 10000;
     const { prePrice, preCount } = stocks[stock];
     let result;
+    let result2;
+    let commission;
+    const preAmount = prePrice * preCount;
     if (type === 1) {
-      result = Number((Number(goal) * preCount - prePrice * preCount) / (Number(curPrice) - Number(goal))).toFixed(0);
+      commission = 5;
+      // 暂不支持佣金计算
+      result = Number((goal * preCount - preAmount) / (curPrice * (1 + commissionRate) - goal))
+        .toFixed(0);
+      result2 = Number((goal * preCount - preAmount - commission) / (curPrice - goal))
+        .toFixed(0);
+      if (result2 > result) {
+        result = result2;
+      }
     } else {
-      result = Number((Number(curPrice) * Number(goal) + prePrice * preCount) / (Number(goal) + preCount)).toFixed(3);
+      const dealAmount = curPrice * goal;
+      commission = dealAmount * commissionRate;
+      if (!isAvoidFive) {
+        if (commission < 5) {
+          commission = 5;
+        }
+      }
+      const allCount = goal + preCount;
+      result = Number((dealAmount + commission + preAmount) / allCount).toFixed(3);
     }
     this.setState({
       result,
@@ -72,7 +96,7 @@ class Stock extends Component {
   render() {
     const { form, history } = this.props;
     const { getFieldProps, getFieldError } = form;
-    const { type, result, stock } = this.state;
+    const { type, result, stock, isAvoidFive } = this.state;
     const stockInfo = stocks[stock];
     return (
       <div className={style.app}>
@@ -96,6 +120,48 @@ class Stock extends Component {
           {getFieldError('stock') ? <div className={style.error}>{getFieldError('stock')}</div> : null}
           <Item extra={(stockInfo && stockInfo.prePrice) || ''}>原成本价</Item>
           <Item extra={(stockInfo && stockInfo.preCount) || ''}>持有数量</Item>
+        </List>
+        <List renderHeader="佣金设置">
+          <InputItem
+            {...getFieldProps('commissionRate', {
+              initialValue: 2.5,
+              rules: [
+                { required: true, message: '请输入佣金比例' },
+              ],
+            })}
+            clear
+            placeholder="请输入佣金比例"
+            type="digit"
+            moneyKeyboardAlign="right"
+          >佣金比例
+          </InputItem>
+          {getFieldError('commissionRate') ? <div className={style.error}>{getFieldError('commissionRate')}</div> : null}
+        </List>
+        <List renderHeader="是否免5">
+          <Item>
+            <Radio
+              className="count"
+              checked={isAvoidFive === 1}
+              onChange={() => {
+                this.setState({
+                  isAvoidFive: 1,
+                });
+              }}
+            >是
+            </Radio>
+            <Radio
+              className="goal"
+              checked={isAvoidFive === 2}
+              onChange={() => {
+                this.setState({
+                  isAvoidFive: 2,
+                });
+              }}
+            >否
+            </Radio>
+          </Item>
+        </List>
+        <List renderHeader="输入当前价格">
           <InputItem
             {...getFieldProps('curPrice', {
               rules: [
@@ -104,7 +170,7 @@ class Stock extends Component {
             })}
             clear
             placeholder="请输入当前价格"
-            type="money"
+            type="digit"
             moneyKeyboardAlign="right"
           >当前价格
           </InputItem>
@@ -141,7 +207,7 @@ class Stock extends Component {
             })}
             clear
             placeholder={`请输入${type === 2 ? '购入手数' : '目标价格'}`}
-            type="money"
+            type="digit"
             moneyKeyboardAlign="right"
           >{type === 2 ? '购入手数' : '目标价格'}
           </InputItem>
@@ -150,8 +216,8 @@ class Stock extends Component {
         <List renderHeader="操作">
           <Item>
             <Flex justify="end">
-              <Button type="primary" size="small" inline onClick={this._submit}>Submit</Button>
-              <Button className={style.btn} type="ghost" size="small" inline onClick={this._reset}>Reset</Button>
+              <Button type="primary" size="small" inline onClick={this._submit}>提交</Button>
+              <Button className={style.btn} type="ghost" size="small" inline onClick={this._reset}>重置</Button>
             </Flex>
           </Item>
         </List>
